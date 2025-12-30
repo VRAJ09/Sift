9429585326b57b4cc08364914fc947a8bf1bbab2
Vraj Patel <vraj.p009@gmail.com>
2025-12-29 23:22:08 -0600

    initial commit of rag pipline script

---
+++ src/rag_chain.py
@@ -0,0 +1,34 @@
+"""
+RAG chain module
+Implements the Retrieval-Augmented Generation pipeline
+"""
+
+# TODO: Implement RAG pipeline
+# This will be implemented in feature/rag-pipeline branch
+
+def create_rag_chain(vector_store, llm):
+    """
+    Create LangChain RAG chain.
+    
+    Args:
+        vector_store: FAISS vector store
+        llm: Language model
+        
+    Returns:
+        RAG chain
+    """
+    pass
+
+def query_rag(question, rag_chain):
+    """
+    Query the RAG chain with a question.
+    
+    Args:
+        question: User question
+        rag_chain: RAG chain
+        
+    Returns:
+        str: Generated answer
+    """
+    pass
+
